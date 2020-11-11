6ae231f39ec1d470eb2aa81ab4ddf177e0af8134
Basma Maachi <bassmamaachi@gmail.com>
2020-11-11 10:50:50 +0000

    delete Authentification service from master branch

--- Authentification/src/main/java/com/banque/auth/service/UserPTO.java
+++
@@ -1,9 +0,0 @@
-package com.banque.auth.service;
-
-import com.banque.auth.controller.payload.request.UserRequest;
-import com.banque.auth.entities.User;
-
-public interface UserPTO {
-
-	public User userProfilePTO(UserRequest userPayload);
-}
