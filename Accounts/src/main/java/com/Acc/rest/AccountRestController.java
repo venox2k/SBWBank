868e7704dffb13d8f3eb74cd48d7a0ec82b352d2
venox2k <walideljaimi@gmail.com>
2020-11-19 03:14:27 +0100

    Update Accounts Api

--- Accounts/src/main/java/com/Acc/rest/AccountRestController.java
+++ Accounts/src/main/java/com/Acc/rest/AccountRestController.java
@@ -1,14 +1,15 @@
 package com.Acc.rest;
 
-
 import java.util.List;
 
 import javax.security.auth.login.AccountNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,86 +22,78 @@ import com.Acc.service.SequenceGenerator;
 @RestController
 @RequestMapping("/Accounts")
 public class AccountRestController {
-	
-	
-	
+
 	@Autowired
 	private AccountService accountService;
-	
+
 	@Autowired
-	private SequenceGenerator sequenceGenerator ;
-	
+	private SequenceGenerator sequenceGenerator;
+
 	/*------ Create Account -------------------------------------------------------------*/
 
 	@PostMapping("/Create")
 	@ResponseBody
-	public  Account insertAccount(@RequestBody Account acc) {
+	public Account insertAccount(@RequestBody Account acc) {
 
 		acc.setId(Long.toString(sequenceGenerator.generateSequence(Account.SEQUENCE_NAME)));
 		accountService.insertAccount(acc);
-		return acc ;
+		return acc;
 
 	}
-	
-	
+
 	/*------ Delete Account -------------------------------------------------------------*/
 
-	   @GetMapping("/Delete/{id}")
-		public  void DeleteAccount(@PathVariable String id)  {
-		
-		
-			accountService.deleteAccount(id);
+	@GetMapping("/Delete")
+	@ResponseBody
+	public String DeleteAccount(@RequestBody String uuid) {
+
+		List<Account> accs = accountService.getAllAccounts(uuid);
+		System.out.println(uuid);
+		for (int  i=0 ; i<accs.size() ;i++) {
+			System.out.println("ui" +accs.get(i).getId());
+			accountService.deleteAccount(accs.get(i).getId());
+		}
+		return "uuid est  : "+uuid ;
+        
+	}
+
+	/*------ Deposit -------------------------------------------------------------------*/
+
+	@PostMapping("/deposit/id={id}&amount={amount}")
+	@ResponseBody
+	public Account deposit(@PathVariable String id, @PathVariable int amount) {
+		System.out.println(accountService.getAccount(id));
+		accountService.deposit(id, amount);
+		return accountService.getAccount(id);
 
 	}
-	
-	
-		/*------ Deposit -------------------------------------------------------------------*/
 
-	
-	    @PostMapping("/deposit/id={id}&amount={amount}")
-		@ResponseBody
-		public  Account deposit(@PathVariable String id , @PathVariable int amount) 
-	    {
-   		   System.out.println(accountService.getAccount(id));
-	    	accountService.deposit(id, amount);
-			return accountService.getAccount(id) ;
+	/*------ Withdraw  -------------------------------------------------------------*/
 
-		}
-	    
-	    
-		/*------ Withdraw  -------------------------------------------------------------*/
+	@PostMapping("/withdraw/id={id}&amount={amount}")
+	@ResponseBody
+	public Account withdraw(@PathVariable String id, @PathVariable int amount) {
+		System.out.println(accountService.getAccount(id));
+		accountService.withdraw(id, amount);
+		return accountService.getAccount(id);
 
-	    
-	    @PostMapping("/withdraw/id={id}&amount={amount}")
-		@ResponseBody
-		public  Account withdraw(@PathVariable String id , @PathVariable int amount) 
-	    {
-   		   System.out.println(accountService.getAccount(id));
-	    	accountService.withdraw(id, amount);
-			return accountService.getAccount(id) ;
+	}
 
-		}
-	    
-	    
-	    
-		/*------ get Account -------------------------------------------------------------*/
+	/*------ get Account -------------------------------------------------------------*/
 
-	    @GetMapping("/get/{id}")
-		@ResponseBody
-		public  Account get(@PathVariable String id ) 
-	    {
-   		   System.out.println(accountService.getAccount(id));
-			return accountService.getAccount(id) ;
+	@GetMapping("/get")
+	@ResponseBody
+	public Account get(@RequestBody String id) {
+		System.out.println(accountService.getAccount(id));
+		return accountService.getAccount(id);
 
-		}
+	}
 
+	@GetMapping("/MyAccounts/{id}")
+	@ResponseBody
+	public List<Account> getAll(@PathVariable String id) {
+		return accountService.getAllAccounts(id);
 
-	    @GetMapping("/MyAccounts/{id}")
-		@ResponseBody
-		public  List<Account> getAll(@PathVariable String id ) 
-	    {
-			return accountService.getAllAccounts(id) ;
+	}
 
-		}
-	    
 }
